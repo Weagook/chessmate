4cf1f65241c5eca20de22dfeea47b42c110e7257
weagook <weagook@gmail.com>
2023-07-04 15:20:29 +0600

    Added cell drawing logic. It does not work completely correctly, you need to fix the rendering and coordinates

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pygame
 from chessmate import *
+from time import sleep
 
 # Create class Cell
 pygame.init()
+print(RES_DIR)
+window = pygame.display.set_mode((800, 600))
+b = Board(window)
 
-window = pygame.display.set_mode((1280, 1024))
-b = Board(window, (0, 0), (0, 0))
+number = 1
+for el in b.cells:
+    print(str(number) + ':', el.position)
+    number += 1
+
+clock = pygame.time.Clock()
+game = True
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    clock.tick(40)
+    b.update()
+    pygame.display.update()
